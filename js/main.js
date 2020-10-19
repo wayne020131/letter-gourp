d86bee0b01ec1511817b027787df1f1f35db39af
vanessa31001 <vanessa31001@gmail.com>
2020-10-19 14:30:21 +0800

    group 整理完成

--- js/main.js
+++ js/main.js
@@ -1,24 +1,9 @@
+$(function(){
+            
+    // hamburger icon 的切換
+    $("button.hamburger").on("click", function(){
+        $(this).toggleClass("is-active");
+        $('.nav_list').slideToggle();
 
-$(document).ready(function() {
-
-	var controller3 = new ScrollMagic.Controller();
-	
-	var wipeAnimations = new TimelineMax()
-		.fromTo('section.panel.black', 1, { x: '100%'	}, { x: '0%', ease: Linear.easeNone})
-		.fromTo('section.panel.green', 1, { x: '100%'	}, { x: '0%', ease: Linear.easeNone})
-		.fromTo('section.panel.yellow', 1, { x: '100%'	}, { x: '0%', ease: Linear.easeNone})
-		.fromTo('section.panel.red', 1, { x: '100%'	}, { x: '0%', ease: Linear.easeNone})
-	    .fromTo('section.panel.purple', 1, { opacity: 0	}, { opacity: 1, ease: Linear.easeNone});
-	
-new ScrollMagic.Scene({
-	triggerElement: '#pin-container',
-	triggerHook: 'onLeave',
-	duration: '500%'
-})
-	.setPin('#pin-container')
-	.setTween(wipeAnimations)
-	.addTo(controller3);
-	
-	
-});
-
+        });
+    });
